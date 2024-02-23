03dc20e9d081e084bf9a522f86947e63567f78be
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-23 19:51:00 +0200

    Initial Commit

---
+++ HowToUseNavigationSplitViewInSwiftUI/HowToUseNavigationSplitViewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseNavigationSplitViewInSwiftUIApp.swift
+//  HowToUseNavigationSplitViewInSwiftUI
+//
+//  Created by test on 23.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseNavigationSplitViewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
